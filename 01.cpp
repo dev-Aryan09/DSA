ee32f2daf0fe6ae3c79468975fedf7f0030e4a2a
AryanSaini <09aryan.lucky@gmail.com>
2024-11-07 22:24:35 +0530

    add practice code file-01

---
+++ 01.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //int -> 4 byte
+    // int num = 50;
+    // cout << num << endl ; 
+    // cout << sizeof(num) << endl;
+
+    //character -> 1 byte
+    // char ch = 'k';
+    // cout << ch << endl;
+    // cout << sizeof(ch) << endl;
+
+    //float -> 4 byte
+    // float point = 1.69;
+    // cout << point << endl;
+    // cout << sizeof(point) << endl;
+
+    //long -> 4 byte
+    // long number = 23;
+    // cout << number << endl;
+    // cout << sizeof(number) << endl;
+
+    // garbage value
+    // int num;
+    // num = 12;
+    // cout << num << endl;
+
+    // int a = 5;
+    // int b = 10;
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << a / b << endl;
+    // cout << a % b << endl;
+
+
+    // int c = 20;
+    // int d = 10;
+
+    // cout << (c > d) << endl;
+    // cout << (c < d) << endl;
+    // cout << (c >= d) << endl;
+    // cout << (c <= d) << endl;
+    // cout << (c != d) << endl;
+    // cout << (c == d) << endl;
+
+    // int age = 20;
+    // int car = 12;
+
+    // if(age >=18 && car >=1) {
+    //     cout << "eligible for license" << endl;
+    // }
+
+    // int marks;
+    
+    // cout << "Enter the marks here: " << endl;
+    // //take input from user 
+    // cin >> marks;
+
+    // cout << "Printing marks: " << marks << endl;
+
+    char ch;
+
+    cin >>ch;
+    cout << "here: " << ch << endl;
+}
