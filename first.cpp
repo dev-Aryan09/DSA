5a759dd1b6879c749923537bf15f5f5a273c1dac
AryanSaini <09aryan.lucky@gmail.com>
2024-11-07 14:01:06 +0530

    add first code in C++

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "love babbar" ;
+    cout << '\n';
+    cout << "aryan saini" ;
+    cout << endl ;
+    cout << "learning DSA" ;
+
+}
