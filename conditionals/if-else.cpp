86fa7d9ce81bfa549d0c1e4b954d4e0c48cd4d91
AryanSaini <09aryan.lucky@gmail.com>
2024-11-14 12:49:54 +0530

    add if else

---
+++ conditionals/if-else.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //     int a;
+    //     cin>>a;
+
+    //     cout << "value of a is:" << a << endl;
+
+    //     // a is positive
+    //     if(a>0) {
+    //         cout << "a is a poitive number" << endl;
+    //     }
+    //     else {
+    //         cout << "a is negative or zero" << endl;
+    // }
+
+    // int a,b;
+
+    // cout << "Enter value of a" << endl;
+    // cin >> a;
+    // cout << "Enter value of b" << endl;
+    // cin >> b;
+
+    // if(a>b) {
+    //     cout << "a is greater" << endl;
+    // }
+    // if(b>a) {
+    //     cout << "b is greater" << endl;
+    // }
+
+    int n;
+
+    cout << "Enter a number" << endl;
+    cin >> n;
+
+    // if (n > 0)
+    // {
+    //     cout << "a is +ve" << endl;
+    // }
+    // else
+    // {
+    //     if (n < 0)
+    //     {
+    //         cout << "a is -ve" << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << "n is zero" << endl;
+    //     }
+    // }
+
+  // more readabulity
+
+    // if (n > 0)             
+    // {
+    //     cout << "a is +ve" << endl;
+    // }
+    // else if (n < 0)
+    // {
+    //     cout << "a is -ve" << endl;
+    // }
+    // else
+    // {
+    //     cout << "n is zero" << endl;
+    // }
+}
