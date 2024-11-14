c2a1fae15bc0ce39eb6f474423e97dbab08f535a
AryanSaini <09aryan.lucky@gmail.com>
2024-11-14 13:00:42 +0530

    add patterns

---
+++ patterns/patterns-01.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "your input is:" << n << endl;
+
+    // pattern-01
+
+    // int i = 1;
+
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << "*";
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // pattern-02
+
+    //  int i = 1;
+    //  while(i<=n){
+
+    //     int j = 1;
+    //     while(j<=n){
+    //         cout << i ;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    //  }
+
+    // pattern-03
+
+    // int i = 1;
+
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << j << " ";
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // pattern-04 (counting upto n)
+
+    //     int i = 1;
+    //     int count = 1;
+
+    //     while (i <= n)
+    //     {
+
+    //         int j = 1;
+    //         while (j <= n)
+    //         {
+    //             cout << count << " ";
+    //             count = count + 1;
+    //             j = j + 1;
+    //         }
+    //         cout << endl;
+    //         i = i + 1;
+    //     }
+
+    // triangle pattern
+
+    // int row = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << "*";
+    //         col = col + 1;
+    //     }
+    //     cout << endl;
+    //     row = row + 1;
+    // }
+
+    // pattern-05
+
+    // int row = 1;
+    // while (row <= n)
+    // {
+
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << row;
+    //         col = col + 1;
+    //     }
+    //     cout << endl;
+    //     row = row + 1;
+    // }
+
+    // pattern-06
+
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << j <<" ";
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // pattern-07
+
+    //     int i = 1;
+    //     int count = 1;
+    //     while (i <= n)
+    //     {
+
+    //         int j = 1;
+    //         while (j <= i)
+    //         {
+    //             cout << count << " ";
+    //             count = count + 1;
+    //             j = j + 1;
+    //         }
+    //         cout << endl;
+    //         i = i + 1;
+    //     }
+
+    // pattern-08
+
+    // int i = 1;
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     int value = i;
+    //     while (j <= i)
+    //     {
+    //         cout << value;
+    //         value = value + 1;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // pattern-09
+
+    // int i = 1;
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << (i-j+1) <<" ";
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+    
+}
