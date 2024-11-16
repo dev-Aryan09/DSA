d6b5ada7ec56295c0f878e2aa010ad9e832b6a9d
AryanSaini <09aryan.lucky@gmail.com>
2024-11-16 22:09:24 +0530

    add left & right shift operator and increment & decrement Operator

--- loops/bitwise-operators.cpp
+++ loops/bitwise-operators.cpp
@@ -4,12 +4,81 @@ using namespace std;
 int main()
 {
 
-    int a = 4;
-    int b = 6;
+    // int a = 4;
+    // int b = 6;
 
-    cout << "a&b :" << (a & b) << endl;
-    cout << "a | b :" << (a | b) << endl;
-    cout << "~a :" << (~a) << endl;
-    cout << "~b :" << (~b) << endl;
-    cout << "a^b :" << (a ^ b) << endl;
+    // cout << "a&b :" << (a & b) << endl;
+    // cout << "a | b :" << (a | b) << endl;
+    // cout << "~a :" << (~a) << endl;
+    // cout << "~b :" << (~b) << endl;
+    // cout << "a^b :" << (a ^ b) << endl;
+
+    // left shift( << ) & right shift( >> ) operator
+
+    // cout << (17>>1) << endl;
+    // cout << (17>>2) << endl;
+    // cout << (19<<1)<< endl;
+    // cout << (19<<2) << endl;
+
+    // increment and decrement operator
+
+    // int i = 7;
+
+    // cout << i++ << endl;      // post increment
+    // // 7, i = 8
+    // cout << ++i << endl;      // pre increment
+    // // 9, i = 9
+    // cout << i-- << endl;      // post decrement
+    // // 8, i = 8
+    // cout << --i << endl;      // pre decrement
+    // // 7, i = 7;
+
+    // Que-01
+
+    // int a, b = 1;
+    // a = 10;
+    // if (++a)
+    //     cout << b;
+    // else
+    //     cout << ++b;
+
+    // Que-02
+
+        // int a = 1;
+        // int b = 2;
+
+        // if(a-- > 0 && ++b > 2) {                    
+        //     cout << "Stage1 - Inside If "<< endl;
+        // } else {
+        //     cout << "Stage2 - Inside else";
+        // }
+        // cout <<"a: "<< a <<"," << "b: "<< b ;
+
+    // Que-03
+
+    //     int a = 1;
+    //     int b = 2;
+
+    //     if(a-- > 0 || ++b > 2) {                       // if first condition is true than second condition will not be executed
+    //         cout << "Stage1 - Inside If "<< endl;
+    //     } else {
+    //         cout << "Stage2 - Inside else";
+    //     }
+
+    // cout <<"a: "<< a <<"," << "b: "<< b ;
+
+    // Que-04
+
+    // int number = 3;
+    // cout << (25 * (++number) );
+
+    // Que-05
+
+    // int a = 1;
+    // int b = a++;
+    // int c = ++a;
+    // cout << b << endl;
+    // ;
+    // cout << c;
+
 }
